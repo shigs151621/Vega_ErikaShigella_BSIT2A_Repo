a9a6ee0ee7f396171900f9f9832c8616142674fa
shigs151621 <verikashigella@gmail.com>
2025-03-10 02:05:16 -0700

    battleship v1

--- battleship/battleship_v1.js
+++ battleship/battleship_v1.js
@@ -1,1 +1,33 @@
-console.log("battleship game");
+var location1 = 3;
+var location2 = 4;
+var location3 = 5;
+
+var guess;
+var hits = 0;
+var guesses = 0;
+
+var isSunk = false;
+
+while (isSunk == false) {
+    guess = prompt("Ready!, Bomba!, Na! 🧨 (Enter number 0-6");
+
+    if (guess < 0 || guess > 6) {
+        alert("Please enter a valid cell number!")
+    } else {
+        guesses = guesses + 1;
+
+        if (guess == location1 || guess == location2 || guess == location3) {
+            hits = hits + 1;
+            alert("Hit! 🎯");
+
+            if (hits == 3) {
+                isSunk = true;
+                alert("Latiberd way naigo! 😒😒")
+            }
+    } else {
+        alert("Way naigo, Boss! 🤦‍♂️")
+    }
+}
+}
+
+var stats = "You sank my battleship in '"
