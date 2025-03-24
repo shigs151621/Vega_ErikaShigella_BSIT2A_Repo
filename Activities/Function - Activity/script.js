ff6103051d75034ce436558bf1847e0bc3067d78
shigs151621 <verikashigella@gmail.com>
2025-03-24 01:36:05 -0700

    Function Acitivity 1

---
+++ Activities/Function - Activity/script.js
@@ -0,0 +1,15 @@
+function whatshallIwear(temp) {
+    if (temp < 60) {
+        console.log("Wear a jacket");
+    }
+    else if (temp < 70) {
+        console.log("Wear a sweater");
+    } else {
+        console.log("Wear t-shirt");
+    }
+    
+}
+
+whatshallIwear(50);
+whatshallIwear(60);
+whatshallIwear(80);
