70166c88a4f5ee5b30b22b9b446ef802c8ddc067
shigs151621 <verikashigella@gmail.com>
2025-03-27 02:12:43 -0700

    practice

---
+++ Practice/function.js
@@ -0,0 +1,19 @@
+// // function calculateArea (r) {
+// // var area;
+// // if (r <= 0) {
+//         return 0;
+//     } else {
+//         area = Math.PI * r * r;
+//         return area;
+//     }
+// }
+
+// var radius = 5.2;
+// var theArea = calculateArea (radius)
+// console.log("The area is:"  + theArea);
+
+function printName (firstName, lastName) {
+    var fullName = firstName + " " + lastName;
+   return fullName;
+} 
+console.log('My Fullname is ' , printName("Erika", "Shigella Vega"))
